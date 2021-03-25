5f5797acf38d7e5ea5768d0018449f6bbd777569
Md Obaidulla <mitulsunny8@gmail.com>
2021-03-25 19:14:02 -0400

    First API project stracture

---
+++ src/main/java/com/osa/files/FileManager.java
@@ -0,0 +1,5 @@
+package com.osa.files;
+
+public class FileManager {
+
+}
---
+++ src/test/java/com/osa/api/APIs.java
@@ -0,0 +1,20 @@
+package com.osa.api;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+public class APIs {
+	public static void main(String[] args) {
+	Response get=GET("/customers/5fb708e8e4a69400176c4366");
+	System.out.println(get.asString());
+	System.out.println(get.jsonPath().getString("firstName"));
+	System.out.println(get.jsonPath().getString("email"));
+	}
+	public static Response GET(String endpoint) {
+		RestAssured.baseURI="https://osasapi.herokuapp.com";
+		RequestSpecification request= RestAssured.given();
+		Response res=request.get(endpoint);
+		return res;
+	}
+}
