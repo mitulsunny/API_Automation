690d130aa196a1a8cbbe909293c094edbfdab3aa
mohammeduddin1980 <73079883+mohammeduddin1980@users.noreply.github.com>
2021-03-26 23:36:55 -0400

    added log4j and property reader

--- src/main/java/com/osa/utility/Utilities.java
+++ src/main/java/com/osa/utility/Utilities.java
@@ -1,5 +1,20 @@
 package com.osa.utility;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
 public class Utilities {
+	
+	public static void main(String[] args) {
+		
+		
+		
+	}
+	
+		public static Logger getLog(Class clazz) {
+		Logger log=Logger.getLogger(clazz);
+		PropertyConfigurator.configure("src/test/resources/log4j.properties");
+		return log;
+		}
 
 }
