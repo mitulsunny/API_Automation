18f251bb46a8bd12d4e387c04cf8ef79fe81a613
Md Obaidulla <mitulsunny8@gmail.com>
2021-03-28 14:07:42 -0400

    Updated APIs and added method in utilis class

--- src/main/java/com/osa/utility/Utilities.java
+++ src/main/java/com/osa/utility/Utilities.java
@@ -2,19 +2,22 @@ package com.osa.utility;
 
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
+import org.testng.Assert;
+
+import com.osa.base.AppContains;
 
 public class Utilities {
-	
-	public static void main(String[] args) {
-		
-		
-		
-	}
-	
-		public static Logger getLog(Class clazz) {
-		Logger log=Logger.getLogger(clazz);
-		PropertyConfigurator.configure("src/test/resources/log4j.properties");
-		return log;
-		}
+
+public static void verifyStatuCode(int expected, int actual) {
+	AppContains.LOG.info("Expected value is: "+expected);
+	AppContains.LOG.info("Acutal value is: "+actual);
+	Assert.assertEquals(actual, expected);
+	AppContains.LOG.info("The actual and expected value is matched. ");
+}
+public static Logger getLog(Class clazz) {
+	Logger log=Logger.getLogger(clazz);
+	PropertyConfigurator.configure("src/test/resources/log4j.properties");
+	return log;
+}
 
 }
