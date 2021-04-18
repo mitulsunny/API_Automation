d5146492fc1690285f84de9928d5a7b5c578f090
Md Obaidulla <mitulsunny8@gmail.com>
2021-04-18 14:55:32 -0400

    added post request

--- src/main/java/com/osa/files/FileManager.java
+++ src/main/java/com/osa/files/FileManager.java
@@ -1,7 +1,38 @@
 package com.osa.files;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+import org.apache.poi.hssf.util.CellReference;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 public class FileManager {
 	
-	
+	public static void main(String[] args) {
+		String value=readExcelFile("myTestData.xlsx","A3");
+		System.out.println(value);
+	}
+	public static String readExcelFile(String fileName,String cellNumber) {
+		String value = null;
+		cellNumber = cellNumber.replace(":", "");
+		try{
+			InputStream file = new FileInputStream(System.getProperty("user.dir")+"\\src\\test\\resources\\TestData\\"+fileName);
+			XSSFWorkbook workBook = new XSSFWorkbook(file);
+			XSSFSheet sheetName = workBook.getSheet("Sheet1");
+			CellReference cf = new CellReference(cellNumber);
+			Row row = sheetName.getRow(cf.getRow());
+			Cell cell = row.getCell(cf.getCol());
+			value = cell.getStringCellValue();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		return value;
+	}
 
 }
--- src/test/java/com/osa/api/APIs.java
+++ src/test/java/com/osa/api/APIs.java
@@ -6,8 +6,10 @@ import java.util.Map.Entry;
 import org.json.JSONObject;
 
 import com.osa.base.AppContains;
+import com.osa.files.FileManager;
 import com.osa.utility.Utilities;
 
+import cucumber.api.DataTable;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
@@ -41,38 +43,40 @@ public class APIs {
 	    String payload_patch=Utilities.getJsonObject(hm1).toString();
 	    
 	    
-	    //POST
-	    AppContains.LOG.info("This is the POST call Payload "+Format.prettyPrint(payload));
-	    Response res_post=executeRequest("post","/customers",payload);
-	    AppContains.LOG.info("This is post response "+Format.prettyPrint(res_post));
-	    
-	    //GET
-	    String get_url="/customers/"+res_post.jsonPath().getString("_id");
-	    AppContains.LOG.info("This is get call URL "+get_url);
-	    Response res_get=executeRequest("Get",get_url);
-	    AppContains.LOG.info("This is get call Response "+Format.prettyPrint(res_get));
-	    
-	    //PATCH
-	    String patch_url="/customers/"+res_get.jsonPath().getString("_id");
-	    AppContains.LOG.info("This is the Patch call Payload "+Format.prettyPrint(payload_patch));
-	    AppContains.LOG.info("This is the Patch call URL "+patch_url);
-	    Response res_patch=executeRequest("patch",patch_url,payload_patch);
-	    AppContains.LOG.info("This is Patch call response "+Format.prettyPrint(res_patch));
-	    
-	    //DELETE
-	    AppContains.LOG.info("This is DELETE call URL "+res_patch); 
-		Response res_delete=executeRequest("delete",patch_url);
-		AppContains.LOG.info("This is DELETE call response "+Format.prettyPrint(res_delete));
+//	    //POST
+//	    AppContains.LOG.info("This is the POST call Payload "+Format.prettyPrint(payload));
+//	    Response res_post=executeRequest("post","/customers",payload);
+//	    AppContains.LOG.info("This is post response "+Format.prettyPrint(res_post));
+//	    
+//	    //GET
+//	    String get_url="/customers/"+res_post.jsonPath().getString("userId");
+//	    AppContains.LOG.info("This is get call URL "+get_url);
+//	    //Response res_get=executeRequest("Get",get_url);
+//	    AppContains.LOG.info("This is get call Response "+Format.prettyPrint(res_get));
+//	    
+//	    //PATCH
+//	    String patch_url="/customers/"+res_get.jsonPath().getString("userId");
+//	    AppContains.LOG.info("This is the Patch call Payload "+Format.prettyPrint(payload_patch));
+//	    AppContains.LOG.info("This is the Patch call URL "+patch_url);
+//	    Response res_patch=executeRequest("patch",patch_url,payload_patch);
+//	    AppContains.LOG.info("This is Patch call response "+Format.prettyPrint(res_patch));
+//	    
+//	    //DELETE
+//	    AppContains.LOG.info("This is DELETE call URL "+res_patch); 
+//		//Response res_delete=executeRequest("delete",patch_url);
+//		AppContains.LOG.info("This is DELETE call response "+Format.prettyPrint(res_delete));
 		}
-	public static Response executeRequest(String request, String url) {
+	public static Response executeRequest(String request, String url, DataTable data) {
+		
 		Response res=null; 
-		String payload=null;
 		String req=request.toLowerCase();
 		if(req.equals("get")) {
 			res=GET(url);
 		}else if(req.equals("post")) {
+		String payload=FileManager.readExcelFile(data.raw().get(1).get(1), data.raw().get(1).get(2));
 			res=POST(url,payload);
 		}else if(req.equals("patch")) {
+		String payload=FileManager.readExcelFile(data.raw().get(1).get(1), data.raw().get(1).get(2));
 			res=PATCH(url,payload);
 		}else if(req.equals("delete")) {
 			res=DELETE(url);
